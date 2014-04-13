bd1c35d206b8e2a5bcfc88bc8ad567b4aad2a422
John Karahalis <john.karahalis@gmail.com>
2014-04-13 13:54:00 -0400

    Add Java solution to problem 1

---
+++ java/Problem1.java
@@ -0,0 +1,21 @@
+public class Problem1 {
+    public static void main(String[] args) {
+        final int LIMIT = 1000;
+
+        int sumThree = sumMultiples(3, LIMIT);
+        int sumFive = sumMultiples(5, LIMIT);
+        int sumCommon = sumMultiples(15, LIMIT);
+
+        int answer =  sumThree + sumFive - sumCommon;
+
+        System.out.println(answer);
+    }
+
+    private static int sumMultiples(int base, int limit) {
+        int sum = 0;
+        for (int i = base; i < limit; i += base) {
+            sum += i;
+        }
+        return sum;
+    }
+}
