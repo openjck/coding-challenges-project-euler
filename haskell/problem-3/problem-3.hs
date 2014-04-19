51b6961e22a07840d1a5cb7e421044733563f476
John Karahalis <john.karahalis@gmail.com>
2014-04-19 14:54:18 -0400

    Add progress towards Haskell solution to problem 3

---
+++ haskell/problem-3/problem-3.hs
@@ -0,0 +1,19 @@
+-- Work in progress. Correctly returns the greatest prime factor, but is not
+-- efficient enough to work with very large numbers.
+
+import Data.List
+import Data.Maybe
+
+gpf :: Integer -> Integer
+gpf n = fromJust (find prime factors)
+  where factors = filter (\x -> x `isFactor` n) [closestHalf, (closestHalf-1)..1]
+        closestHalf = n `div` 2
+
+prime :: Integer -> Bool
+prime n = none (\x -> x `isFactor` n) [2..((floor . sqrt . fromIntegral) n)]
+
+isFactor :: Integer -> Integer -> Bool
+isFactor divisor dividend = dividend `mod` divisor == 0
+
+none :: (a -> Bool) -> [a] -> Bool
+none f l = not (any f l)
