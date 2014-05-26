d21c4bf0ebf0d6993844eadbb7062613c5b6d26d
John Karahalis <john.karahalis@gmail.com>
2014-05-25 23:17:15 -0400

    Haskell #7: Formatting

--- haskell/problem-7/problem-7.hs
+++ haskell/problem-7/problem-7.hs
@@ -12,11 +12,10 @@ primes = sieve 2 [3..]
 
 -- Sieve of Eratosthenes
 sieve :: Integer -> [Integer] -> [Integer]
-sieve n candidates =
-    n : (sieve nextPrime remainingCandidates)
-        where nextPrime = head candidates
-              remainingCandidates = filter (\x -> not (n `isFactorOf` x)) candidatesLessN
-              candidatesLessN = drop 1 candidates
+sieve n candidates = n : (sieve nextPrime remainingCandidates)
+    where nextPrime = head candidates
+          remainingCandidates = filter (\x -> not (n `isFactorOf` x)) candidatesLessN
+          candidatesLessN = drop 1 candidates
 
 isFactorOf :: Integer -> Integer -> Bool
 isFactorOf divisor dividend = dividend `mod` divisor == 0
