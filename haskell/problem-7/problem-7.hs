7e0c5b8904acd2fa1c08bb563a50db31c67ed78a
John Karahalis <john.karahalis@gmail.com>
2014-06-01 20:21:38 -0400

    Haskell #7: Remove unneeded isFactorOf function

--- haskell/problem-7/problem-7.hs
+++ haskell/problem-7/problem-7.hs
@@ -8,6 +8,3 @@ answer = nthPrime 10001
 
 nthPrime :: Int -> Integer
 nthPrime n = (primes sieveOfEratosthenes) !! (n - 1)
-
-isFactorOf :: Integer -> Integer -> Bool
-isFactorOf divisor dividend = dividend `mod` divisor == 0
