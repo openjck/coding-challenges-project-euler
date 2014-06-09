37e9bf034f451daee7d43bae5d296fcd4384d8e9
John Karahalis <john.karahalis@gmail.com>
2014-06-08 22:59:18 -0400

    Haskell: Reformat all source to four-space indents

--- haskell/problem-2/problem-2.hs
+++ haskell/problem-2/problem-2.hs
@@ -6,6 +6,6 @@ answer = sum . filter even . takeWhile (< 4000000) $ fibList
 
 fibList :: [Integer]
 fibList = map fib [0..]
-  where fib 0 = 0
-        fib 1 = 1
-        fib n = (fibList !! (n - 2)) + (fibList !! (n - 1))
+    where fib 0 = 0
+          fib 1 = 1
+          fib n = (fibList !! (n - 2)) + (fibList !! (n - 1))
